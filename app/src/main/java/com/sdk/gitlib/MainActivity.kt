baa353c911fc96893579a807a6c96a74014462b8
samandar-mee <samandarcoderdev@gmail.com>
2022-11-24 12:22:53 +0500

    Initial Commit

---
+++ app/src/main/java/com/sdk/gitlib/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.sdk.gitlib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sdk.check_internet.CheckInternet
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
